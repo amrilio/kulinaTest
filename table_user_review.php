e514554cb78d8cd64f837de394bd8382c93c75d4
amrilio <tmarry2@gmail.com>
2018-06-15 02:01:55 +0700

    Create table_user_review.php

---
+++ table_user_review.php
@@ -0,0 +1,10 @@
+CREATE TABLE user_review (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+order_id VARCHAR(10) NOT NULL,
+product_id VARCHAR(10) NOT NULL,
+user_id VARCHAR(10) NOT NULL,
+rating FLOAT,
+review TEXT,
+created_at TIMESTAMP,
+updated_at TIMESTAMP
+)
